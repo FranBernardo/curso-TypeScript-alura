673158a203a277c784c3177c08b3c183d115e0c7
Franciele <francielebernardo88@hotmail.com>
2022-06-29 14:39:56 -0300

    programação preventiva

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,23 +1,28 @@
 export class Negociacao {
     // no typescript da para fazer dessa forma.
     constructor(
-        private _data: Date, 
-        private _quantidade: number, 
-        private _valor:number
+        private  _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor:number
         ) {}
+        // quando add o readonly ele tornar o metodo so leitura assim nao precisamos mais do get.
+    // get data(){
+    //     return this._data
+    // }
 
-    get data(){
-        return this._data
-    }
+    // get quantidade(){
+    //     return this._quantidade
+    // }
+    // get valor(){
+    //     return this._valor;
+    // }
 
-    get quantidade(){
-        return this._quantidade
-    }
-    get valor(){
-        return this._valor;
+    get data(): Date{
+        const data = new Date(this._data.getTime())
+        return data
     }
 
     get Volume() {
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     }
 }
